5b09d8fa00dbb62f8f6b651f59cce09072aa952b
Leader <abdo.gammal1233@gmail.com>
2024-08-10 16:07:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import LayOut from './Components/LayOut/LayOut'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './Components/About/About'
+import Portfolio from './Components/Portfolio/Portfolio'
+import Contact from './Components/Contact/Contact'
+import Framwork from './Components/Framwork/Framwork'
+import NotFound from './Components/NotFound/NotFound'
+
+function App() {
+  
+  let x = createBrowserRouter([
+    {path:"taskOne/", element: <LayOut />, children: [
+      {path:"About", element: <About />},
+      {path:"Portfolio", element: <Portfolio />},
+      {path:"Contact", element: <Contact />},
+      {index: true, element: <Framwork />},
+      {path:"*", element: <NotFound />}
+    ]}
+  ])
+  
+  return (
+    <RouterProvider router={x}>
+    <LayOut />
+    </RouterProvider>
+  )
+}
+
+export default App
